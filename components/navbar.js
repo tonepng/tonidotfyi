a95509115eeeaa190e402f2a513890d2c4eaa8bb
tone <5797963+ToniTwoShoes@users.noreply.github.com>
2022-07-27 00:39:06 -0700

    colours2

--- components/navbar.js
+++ components/navbar.js
@@ -25,7 +25,7 @@ const LinkItem = ({ href, path, target, children, ...props }) => {
     <NextLink href={href} passHref scroll={false}>
       <Link
         p={2}
-        bg={active ? 'grassTeal' : undefined}
+        bg={active ? 'smokeGrey' : undefined}
         color={active ? '#202023' : inactiveColor}
         target={target}
         {...props}
