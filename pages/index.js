5c5bc187c3fb543d84b94d1e29fca0fac087c986
tone <5797963+ToniTwoShoes@users.noreply.github.com>
2022-07-27 16:38:53 -0700

    asdas

--- pages/index.js
+++ pages/index.js
@@ -43,6 +43,12 @@ const Home = () => (
 
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
+          borderRadius="lg"
+          mb={6}
+          p={3}
+          textAlign="center"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          css={{ backdropFilter: 'blur(10px)' }}
           <Heading as="h2" variant="page-title">
             Antoni Alvarez
           </Heading>
