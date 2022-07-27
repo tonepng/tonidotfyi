de102d43b1e7af5f42ee1d9126672df53650ef46
tone <5797963+ToniTwoShoes@users.noreply.github.com>
2022-07-26 23:21:35 -0700

    change page finally?

--- components/navbar.js
+++ components/navbar.js
@@ -71,10 +71,10 @@ const Navbar = props => {
           flexGrow={1}
           mt={{ base: 4, md: 0 }}
         >
-          <LinkItem href="/works" path={path}>
+          <LinkItem href="/ilm" path={path}>
             ILM Application
           </LinkItem>
-          <LinkItem href="/posts" path={path}>
+          <LinkItem href="/works" path={path}>
             Works
           </LinkItem>
           <LinkItem
